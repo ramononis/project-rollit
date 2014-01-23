64b9ccad83fcbe2261986d702875b785ecaa4dd0
swordiemen <swordiemen@live.nl>
2014-01-23 02:26:08 -0800

    Added testServer files.

---
+++ testServer/Peer.java
@@ -0,0 +1,125 @@
+package ss.week7.cmdline;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Peer for a simple client-server application
+ * 
+ * @author Theo Ruys
+ * @version 2005.02.21
+ */
+public class Peer implements Runnable {
+	public static final String EXIT = "exit";
+
+	protected String name;
+	protected Socket sock;
+	protected BufferedReader in;
+	protected BufferedWriter out;
+	
+	private String stream;
+	protected Scanner sc;
+
+	/*
+	 * @ requires (nameArg != null) && (sockArg != null);
+	 */
+	/**
+	 * Constructor. creates a peer object based in the given parameters.
+	 * 
+	 * @param nameArg
+	 *            name of the Peer-proces
+	 * @param sockArg
+	 *            Socket of the Peer-proces
+	 */
+	public Peer(String nameArg, Socket sockArg) throws IOException {
+		this.name = nameArg;
+		this.sock = sockArg;
+		this.in = new BufferedReader(new InputStreamReader(
+					sockArg.getInputStream()));
+		this.out = new BufferedWriter(new OutputStreamWriter(
+				sockArg.getOutputStream()));
+	}
+
+	/**
+	 * Reads strings of the stream of the socket-connection and writes the
+	 * characters to the default output
+	 */
+	public void run() {
+		try {
+			while(true){
+			stream = in.readLine();
+			System.out.println(stream);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reads a string from the console and sends this string over the
+	 * socket-connection to the Peer proces. On Peer.EXIT the method ends
+	 */
+	public void handleTerminalInput() {
+		sc = new Scanner(System.in);
+		boolean exit = false;
+		while (sc.hasNextLine() && !exit) {
+			String line = sc.nextLine();
+			
+			if (line.equals("exit")) {
+				exit = true;
+				System.out.println("Shutting off...");
+			} else {
+				try {
+					System.out.println("Verstuur: " + line);
+					out.write(line + "\n");
+					out.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Closes the connection, the sockets will be terminated
+	 */
+	public void shutDown() {
+		try {
+			
+			in.close();
+			out.close();
+			sock.close();
+			System.out.println("Exited.");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/** returns name of the peer object */
+	public String getName() {
+		return name;
+	}
+
+	/** read a line from the default input */
+	static public String readString(String tekst) {
+		System.out.print(tekst);
+		String antw = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			antw = in.readLine();
+		} catch (IOException e) {
+		}
+
+		return (antw == null) ? "" : antw;
+	}
+}
